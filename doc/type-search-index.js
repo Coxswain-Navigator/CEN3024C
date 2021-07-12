7a163a5e81e66b5dfa02ff27701c5640b49985f4
Coxswain <35991533+Coxswain-Navigator@users.noreply.github.com>
2021-07-11 22:26:50 -0400

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.eclipse.wb.swt","l":"Readertest"},{"p":"<Unnamed>","l":"ReaderTextReader"},{"p":"org.eclipse.wb.swt","l":"SWTResourceManager"}];updateSearchResults();
